9c644fed0aba5d25c1b26333dd3d3d2e0ce492d0
Jefferson Felipe <felipej2626@gmail.com>
2017-07-23 10:55:22 -0300

    Criação da Tabela Usuarios

---
+++ api/models/Usuarios.js
@@ -0,0 +1,31 @@
+/**
+ * Usuarios.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nome: {
+      type: 'string',
+			size: 1000
+    },
+
+    email: {
+      type: 'string',
+			size: 1000
+    },
+
+    ocupacao: {
+      type: 'string',
+			size: 1000
+    },
+
+    idade: {
+      type: 'integer'
+    }
+  }
+};
+
