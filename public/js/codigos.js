8d50a488caf4650f2c6cdbd8a81de7f1cafac3f2
Jefferson Felipe <felipej2626@gmail.com>
2017-07-24 00:56:41 -0300

    Consertos e Ajustes
    -Conserto para pegar o IP do usuario;

--- public/js/codigos.js
+++ public/js/codigos.js
@@ -5,13 +5,12 @@ function gravar() {
     var sobrenome = document.getElementById("inputSobrenome");
     var email = document.getElementById("inputEmail");
     var ocupacao = document.getElementById("inputOcupacao");
-    var ip = document.getElementById("inputIP");
 
     var params = "nome=" + nome.value +
     "&sobrenome=" + sobrenome.value + 
     "&email=" + email.value +
     "&ocupacao=" + ocupacao.value +
-    "&ip=" + ip.value;
+    "&ip=" + ip;
 
     xhr.open('POST', 'usuarios', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -22,9 +21,9 @@ function gravar() {
     sobrenome.innerHTML = "";
     email.innerHTML = "";
     ocupacao.innerHTML = "";
-    ip.innerHTML = "";
 }
 
+var ip;
 
 $(document).ready(function ubsrt()
 {
@@ -36,12 +35,8 @@ $(document).ready(function ubsrt()
 	pc.createOffer(pc.setLocalDescription.bind(pc), noop);   
     pc.onicecandidate = function(ice){ 
    	    if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
-        var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-        
-        //document.getElementById("inputIP").innerHTML = myIP;
-        $("inputIP").html(myIP);
-        $("inputIP").text(myIP);
-        alert(myIP);
+        ip = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+
         pc.onicecandidate = noop;
     }; 
 });
