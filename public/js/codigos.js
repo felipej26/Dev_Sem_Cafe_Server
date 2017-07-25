12637cf9f5592862d6c2b73652032b5d5efc384f
Jefferson Felipe <felipej2626@gmail.com>
2017-07-25 03:22:08 -0300

    Teste com link completo

--- public/js/codigos.js
+++ public/js/codigos.js
@@ -10,7 +10,7 @@ function gravar() {
     "&email=" + email.value +
     "&ip=" + ip;
 
-    xhr.open('POST', 'usuarios/', false);
+    xhr.open('POST', 'http://www.devsemcafe.com.br/usuarios/', false);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.onload = function () { };
     xhr.send(params);
